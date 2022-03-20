b11b3098365057463a3fd9b3f80edd478e7a267e
王浩 <2360884227@qq.com>
2022-03-20 20:20:46 +0800

    2

--- 源.c
+++ 源.c
@@ -2,35 +2,13 @@
 
 int main()
 {
-    int price = 0;
-
-    printf("请输入金额（元）：");
-    scanf_s("%d", &price);
-
-    int change = 100 - price;
-
-    printf("%d\n", change);
-   kk();
-
-    return 0;
+	EC(1,6);
 }
-extern int kk()
+int EC(int a, int b)
 {
-    int a;
-    int b;
-
-   
-    scanf_s("%d %d",&a,&b);
-    if(a>b)
-    {
-        printf("两个数中大的是%d",a);
-
-    }
-    else
-    {
-        printf("两个数中大的是%d",b);
-
-    }
-
-
-} 
+	int t;
+	t = a;
+	a = b;
+	b = t;
+	printf("%d %d", a, b);
+}
