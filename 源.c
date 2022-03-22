e16a52f01e401a4de462df113b47c4f351e89228
王浩 <2360884227@qq.com>
2022-03-22 22:26:54 +0800

    point

--- 源.c
+++ 源.c
@@ -53,11 +53,13 @@
 #include <stdlib.h>
 enum color {
 	red=4,blue,grey,black,green
-};
+}; 
+const int MAX = 3;
+const int MIN = 0;
 enum color dd;
 int main()
 {
-    printf("请输入你喜欢的颜色");
+    /*printf("请输入你喜欢的颜色");
 	scanf_s("%d", &dd);
 	switch(dd)
     {
@@ -74,5 +76,54 @@ int main()
         printf("你没有选择你喜欢的颜色");
     }
 
+    return 0;*/
+    //int a=11;
+    //int* p;
+    //char* s;
+    //int sq;
+    //p = &a;
+    //s =11;
+    //printf("a的指针为%p\n", p);
+    //printf("a的指针为%p\n", s);
+    //int  var = 20;   /* 实际变量的声明 */
+    //int* ip;        /* 指针变量的声明 */
+
+    //ip = &var;  /* 在指针变量中存储 var 的地址 */
+
+    //printf("var 变量的地址: %p\n", &var);
+
+    ///* 在指针变量中存储的地址 */
+    //printf("ip 变量存储的地址: %p\n", ip);
+
+    ///* 使用指针访问值 */
+    //printf("*ip 变量的值: %d\n", *ip);
+    //int* ptr = NULL;
+
+    //printf("ptr 的地址是 %p\n", ptr);
+
+    //return 0;
+    int i;
+
+    int ss[] = { 100,200,300 };
+    int* p;
+    p = ss;
+    for (i = 0; i < MAX; i++)
+    {
+        printf("ss的地址%p\n", ss);
+        printf("ss的值%d\n", *p);
+        p++;
+    }
     return 0;
+
+
+
+
+
+
+
+
+
+
+
+
 }
